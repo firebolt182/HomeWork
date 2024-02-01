a064385f120fd72aa85c9a01108b4121373ac3b4
firebolt182 <firebolt182@mail.ru>
2024-02-01 23:07:13 +0300

    module2.homework3

--- src/firebolt/example/module2/homework3/ex2/Garden.java
+++ src/firebolt/example/module2/homework3/ex2/Garden.java
@@ -1,5 +1,6 @@
 package firebolt.example.module2.homework3.ex2;
 
+@FunctionalInterface
 public interface Garden<T extends Fruit> {
     T grow();
 }
--- src/firebolt/example/module2/homework3/ex5/Ruler.java
+++ src/firebolt/example/module2/homework3/ex5/Ruler.java
@@ -1,5 +1,6 @@
 package firebolt.example.module2.homework3.ex5;
 
+@FunctionalInterface
 public interface Ruler<T, O> {
     public O action(T object);
 }
